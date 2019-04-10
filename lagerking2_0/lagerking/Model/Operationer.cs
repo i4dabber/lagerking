95841757d83b2e44ff9eed93e36d379a49547585
i4dabber <43787841+i4dabber@users.noreply.github.com>
2019-04-10 12:57:28 +0200

    Add files via upload

---
+++ lagerking2_0/lagerking/Model/Operationer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lagerking.Model
+{
+    public class Operationer
+    {
+        string navn;
+        string pris;
+        string antal;
+        string afdeling;
+
+        public Operationer() { }
+       
+
+        public  Operationer(string aNavn, string aPris, string aAntal, string aAfdeling)
+        {
+           navn = aNavn;
+            pris = aPris;
+            antal = aAntal;
+            afdeling = aAfdeling;
+        }
+
+        public string NAVN { get; set; }
+
+        public string PRIS { get; set; }
+
+        public string ANTAL { get; set; }
+
+        public string AFDELING { get; set; }
+      
+
+       
+
+    }
+}
---
+++ lagerking2_0/lagerking/ViewModel/Command.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using lagerking.Model;
+using System.Windows.Input;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Xml.Serialization;
+using System.IO;
+using System.Windows.Media;
+using System.Windows.Data;
+using Microsoft.Win32;
+
+namespace lagerking.ViewModel
+{
+    public class Command : ObservableCollection<Operationer>, INotifyPropertyChanged
+    {
+        ICommand _addCommand;
+        public ICommand AddCommand
+        {
+            get { return _addCommand ?? (_addCommand = new RelayCommand(AddAgent)); }
+        }
+
+        private void AddAgent()
+        {
+
+            Operationer newOps = new Operationer();
+            Add(newOps);
+
+        }
+
+
+    }
+
+}
